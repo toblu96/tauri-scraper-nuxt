f34c465c4e5aabfd06f69942626dee9efcf16dad
= <=>
2022-10-12 13:55:02 +0200

    Add Tailwind, TailwindUI, HeadlessUI, Heroicons

--- app.vue
+++ app.vue
@@ -1,29 +1,40 @@
 <script setup>
-import { invoke } from '@tauri-apps/api'
-import { Store } from 'tauri-plugin-store-api';
+import { invoke } from "@tauri-apps/api";
+import { Store } from "tauri-plugin-store-api";
 
-const message = ref("")
-const storeVal = ref(0)
+const message = ref("");
+const storeVal = ref(0);
+const store = new Store("settings.dat");
 
 async function greet(event) {
-  message.value = await invoke("greet", { name: 'toubi' })
+  message.value = await invoke("greet", { name: "toubi" });
 }
 
 async function createStore(event) {
-  const store = new Store('settings.dat');
-  await store.get("data")
-  await store.set('some-key', { value: 5 });
-  storeVal.value = await store.get('some-key');
+  await store.get("data");
+  await store.set("some-key", { value: 5 });
+  storeVal.value = await store.get("some-key");
 }
 
+async function setStoreValue(event) {
+  await store.set("some-key", { value: 3 });
+}
 
+async function getStoreValue(event) {
+  storeVal.value = await store.get("some-key");
+}
 </script>
 <template>
   <div>
+    <Stats />
     <button @click="greet()">Greet</button>
     <button @click="createStore()">Create Store</button>
-    <p>{{message}}</p>
-    {{storeVal}}
+    <div class="m-4 space-x-4 border-l bg-green-50">
+      <button @click="setStoreValue()">set Store</button>
+      <button @click="getStoreValue()">get Store</button>
+    </div>
+    <p>{{ message }}</p>
+    {{ storeVal }}
     <NuxtWelcome />
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
+    `App.{js,ts,vue}`,
+    `app.{js,ts,vue}`
+  ]
+}
