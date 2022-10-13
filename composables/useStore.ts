8b7d99b0e05fe63f0ea1d4f691266dfaf9dd9fb3
= <=>
2022-10-13 14:30:39 +0200

    Remove unused logger

--- composables/useStore.ts
+++ composables/useStore.ts
@@ -1,8 +1,6 @@
 import { Store } from "tauri-plugin-store-api";
 const store = new Store("settings.dat");
-console.log("composable once")
 
 export const useStore = () => {
-    console.log("composable")
     return store
 }
