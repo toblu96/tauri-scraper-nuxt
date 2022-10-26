31a7bb51227eea7372e4f18ee801530fcb24de56
= <=>
2022-10-26 09:08:13 +0200

    minor styling changes

--- pages/settings.vue
+++ pages/settings.vue
@@ -39,37 +39,39 @@ watchEffect(async () => {
           available after restart.
         </p>
       </div>
-      <ul role="list" class="mt-2 divide-y divide-gray-200">
-        <SwitchGroup as="li" class="flex items-center justify-between py-4">
-          <div class="flex flex-col">
-            <SwitchLabel
-              as="p"
-              class="text-sm font-medium text-gray-900"
-              passive
-              >Autostart App</SwitchLabel
-            >
-            <SwitchDescription class="text-sm text-gray-500"
-              >If enabled, this application will start on each server restart
-              automatically.</SwitchDescription
-            >
-          </div>
-          <Switch
-            v-model="autostartEnabled"
-            :class="[
-              autostartEnabled ? 'bg-indigo-500' : 'bg-gray-200',
-              'relative ml-4 inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
-            ]"
-          >
-            <span
-              aria-hidden="true"
+      <div class="mt-2 bg-white px-4 py-2 shadow sm:rounded-lg sm:px-6">
+        <ul role="list" class="divide-y divide-gray-200">
+          <SwitchGroup as="li" class="flex items-center justify-between py-4">
+            <div class="flex flex-col">
+              <SwitchLabel
+                as="p"
+                class="text-sm font-medium text-gray-900"
+                passive
+                >Autostart App</SwitchLabel
+              >
+              <SwitchDescription class="text-sm text-gray-500"
+                >If enabled, this application will start on each server restart
+                automatically.</SwitchDescription
+              >
+            </div>
+            <Switch
+              v-model="autostartEnabled"
               :class="[
-                autostartEnabled ? 'translate-x-5' : 'translate-x-0',
-                'inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
+                autostartEnabled ? 'bg-indigo-500' : 'bg-gray-200',
+                'relative ml-4 inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
               ]"
-            />
-          </Switch>
-        </SwitchGroup>
-      </ul>
+            >
+              <span
+                aria-hidden="true"
+                :class="[
+                  autostartEnabled ? 'translate-x-5' : 'translate-x-0',
+                  'inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
+                ]"
+              />
+            </Switch>
+          </SwitchGroup>
+        </ul>
+      </div>
     </div>
     <section class="space-y-4 p-6">
       <h2 class="text-lg font-medium leading-6 text-gray-900">
