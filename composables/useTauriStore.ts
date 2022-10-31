1d3631e4e62540bd08e4b619d8957c6aaf627ff2
= <=>
2022-10-31 11:33:22 +0100

    change default store path to global windows path 'ProgramData'

--- composables/useTauriStore.ts
+++ composables/useTauriStore.ts
@@ -1,5 +1,8 @@
 import { Store } from "tauri-plugin-store-api";
-const store = new Store("settings.dat");
+import { getName } from "@tauri-apps/api/app";
+
+const appName = await getName();
+const store = new Store(`C:\\ProgramData\\Tauri\\${appName}\\settings.dat`);
 
 export const useTauriStore = () => {
     return store
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,11 @@ export default defineNuxtConfig({
     alias: {
         // due to https://github.com/nuxt/framework/issues/6623
         // pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
+    },
+    vite: {
+        build: {
+            target: "esnext"
+        }
     }
 
 })
