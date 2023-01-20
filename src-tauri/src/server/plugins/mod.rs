a67281bbbc0f2439070db2d7fecb8ae587762332
= <=>
2023-01-20 15:04:36 +0100

    Get file versions on application start

--- src-tauri/src/server/plugins/mod.rs
+++ src-tauri/src/server/plugins/mod.rs
@@ -16,59 +16,80 @@ pub fn init(app_state: Arc<AppState>) {
     let (tx, mut rx) = broadcast::channel::<String>(40);
     let tx_file_watcher = Arc::new(RwLock::new(tx.clone()));
 
+    // check all enabled file versions on application start
+    let files = app_state
+        .db
+        .read()
+        .unwrap()
+        .get_unwrap::<Files>("files")
+        .unwrap();
+    for (_uuid, file) in files {
+        // skip disabled file watchers
+        if !&file.enabled {
+            continue;
+        }
+        handle_file_change(&file.path, &app_state);
+    }
+
     // init file listener
     let mut file_watcher = file_watcher::FileWatcher::init(tx_file_watcher, &app_state);
 
     // Create global listener - execute version and mqtt logic here
     tokio::spawn(async move {
-        while let Ok(msg) = rx.recv().await {
+        while let Ok(path) = rx.recv().await {
             // handle local db changes
             let db_path = crate::server::store::FILE_DB_PATH;
             let db_name = crate::server::store::FILE_DB_NAME;
             let db_string = format!("{db_path}/{db_name}.kv");
-            if msg == db_string {
+            if path == db_string {
                 file_watcher.refresh();
                 continue;
             }
 
             // TODO: handle other file changes
-            // handle different file types
-            if let Some(os_str) = Path::new(&msg).extension() {
-                if let Some(extension) = os_str.to_str() {
-                    match extension {
-                        "exe" | "dll" => {
-                            // get file version from file properties
-                            let file_version =
-                                file_version_reader::get_file_version_from_file_properties(&msg);
-                            match file_version {
-                                Ok(version) => {
-                                    update_file_version(&app_state, msg.clone(), version.clone());
-                                    // TODO: send mqtt message
-                                }
-                                Err(err) => {
-                                    println!("Could not get file version due to: {err:?}")
-                                    // TODO: Update file state with error message
-                                }
-                            }
-                        }
-                        _ => {
-                            // get current time stamp and file hash - no file version available
-                            let hash = file_version_reader::get_file_meta_hash(&msg);
-                            match hash {
-                                Ok(hash) => {
-                                    update_file_version(&app_state, msg.clone(), hash.clone());
-                                    // TODO: send mqtt message
-                                }
-                                Err(err) => println!("Could not get file version due to: {err:?}"),
-                            }
-                        }
-                    }
-                }
-            }
+            handle_file_change(&path, &app_state);
         }
     });
 }
 
+/// Gets the new file version on file change and stores it to the local DB
+fn handle_file_change(path: &String, app_state: &Arc<AppState>) {
+    if let Some(os_str) = Path::new(&path).extension() {
+        if let Some(extension) = os_str.to_str() {
+            // handle different file types
+            match extension {
+                "exe" | "dll" => {
+                    // get file version from file properties
+                    let file_version =
+                        file_version_reader::get_file_version_from_file_properties(&path);
+                    match file_version {
+                        Ok(version) => {
+                            update_file_version(&app_state, path.clone(), version.clone());
+                            // TODO: send mqtt message
+                        }
+                        Err(err) => {
+                            println!("Could not get file version due to: {err:?}")
+                            // TODO: Update file state with error message
+                        }
+                    }
+                }
+                _ => {
+                    // get current time stamp and file hash - no file version available
+                    let hash = file_version_reader::get_file_meta_hash(&path);
+                    match hash {
+                        Ok(hash) => {
+                            update_file_version(&app_state, path.clone(), hash.clone());
+                            // TODO: send mqtt message
+                        }
+                        Err(err) => println!("Could not get file version due to: {err:?}"),
+                    }
+                }
+            }
+        }
+    }
+}
+
+/// write the new file version to the local DB
 fn update_file_version(app_state: &Arc<AppState>, path: String, version: String) {
     // Update file state with version
     let mut files = app_state
