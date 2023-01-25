9df73eeedb61fe1d5a8f34004e3303f7fbc85528
= <=>
2023-01-25 10:17:38 +0100

    Improve tauri fetch with default values

--- composables/useTauriFetch.ts
+++ composables/useTauriFetch.ts
@@ -3,8 +3,9 @@ import { fetch as tauriFetch, FetchOptions } from "@tauri-apps/api/http";
 // CORS workaround because nuxt serves as SPA here
 export const useTauriFetch = () => {
   const fetch = async (path: string, options?: FetchOptions) => {
+    const opts: FetchOptions = { method: "GET", timeout: 1, ...options };
     try {
-      let resp = await tauriFetch(`http://localhost:8000/api${path}`, options);
+      let resp = await tauriFetch(`http://localhost:8000/api${path}`, opts);
 
       return { ...resp };
     } catch (error) {
