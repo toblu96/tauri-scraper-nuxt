46405aa8d87e25ed3a3da5e37eea31c303581cc5
= <=>
2023-01-25 12:19:10 +0100

    Update dashboard page to new backend

--- components/list/scraper-info.vue
+++ components/list/scraper-info.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="overflow-hidden bg-white shadow sm:rounded-md">
     <ul role="list" class="divide-y divide-gray-200">
-      <li class="px-4 py-4 sm:px-6">
+      <div class="px-4 py-4 sm:px-6">
         <h2 class="text-lg font-medium leading-6 text-gray-900">
           Version Scrapers
         </h2>
-      </li>
+      </div>
+      <!-- empty state -->
+      <div v-if="scrapers.length == 0" class="px-4 py-12 sm:px-6">
+        <div class="text-center">
+          <DocumentTextIcon
+            class="mx-auto h-12 w-12 stroke-1 text-gray-400"
+            aria-hidden="true"
+          />
+          <h3 class="mt-2 text-sm font-medium text-gray-900">
+            No files activated
+          </h3>
+          <p class="mt-1 text-sm text-gray-500">
+            Configure and activate at least one file to get some version changes
+            monitored.
+          </p>
+          <div class="mt-6">
+            <NuxtLink
+              to="/scrapers"
+              class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              File Configuration
+              <ArrowRightIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+      <!-- files list -->
       <li v-for="scraper in scrapers" :key="scraper.id">
         <NuxtLink
           :to="`/scrapers/${scraper.id}/edit`"
@@ -19,27 +45,31 @@
               <div class="ml-2 flex flex-shrink-0">
                 <p
                   :class="
-                    scraper.updateState === 'Successful'
+                    scraper.update_state === 'Success'
                       ? 'bg-green-100 text-green-800'
                       : 'bg-red-100 text-red-800'
                   "
                   class="truncate rounded-full px-2 text-xs font-semibold leading-5"
                 >
-                  {{ scraper.updateState || "no data" }}
+                  {{ scraper.update_state || "no data" }}
                 </p>
               </div>
             </div>
             <div class="mt-2 sm:flex sm:justify-between">
-              <div class="sm:flex">
-                <p class="flex items-center text-sm text-gray-500">
+              <div class="lg:flex">
+                <div
+                  class="flex w-full items-center text-sm text-gray-500 sm:w-52 sm:max-w-sm md:w-full"
+                >
                   <HashtagIcon
                     class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
                     aria-hidden="true"
                   />
-                  {{ scraper.lastVersion || "no data" }}
-                </p>
+                  <p class="truncate">
+                    {{ scraper.last_version || "no data" }}
+                  </p>
+                </div>
                 <p
-                  class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6"
+                  class="mt-2 flex items-center text-sm text-gray-500 lg:mt-0 lg:ml-6"
                 >
                   <DocumentMagnifyingGlassIcon
                     class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
@@ -56,8 +86,8 @@
                 <p>
                   Last updated on
                   {{ " " }}
-                  <time :datetime="scraper.lastUpdateUTC">{{
-                    new Date(scraper.lastUpdateUTC).toLocaleString()
+                  <time :datetime="scraper.last_update_utc">{{
+                    new Date(scraper.last_update_utc as string).toLocaleString()
                   }}</time>
                 </p>
               </div>
@@ -74,16 +104,18 @@ import {
   ClockIcon,
   HashtagIcon,
   DocumentMagnifyingGlassIcon,
+  ArrowRightIcon,
 } from "@heroicons/vue/20/solid";
+import { DocumentTextIcon } from "@heroicons/vue/24/outline";
 
 type ScraperProps = {
   id: string;
   name: string;
   enabled: boolean;
   path: string;
-  lastUpdateUTC?: string;
-  updateState?: string;
-  lastVersion?: string;
+  last_update_utc?: string;
+  update_state?: string;
+  last_version?: string;
 };
 
 const props = defineProps<{
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,33 @@
-<script setup>
-import { useScraperStore } from "~~/stores/scrapers";
-const store = useScraperStore();
+<script setup lang="ts">
+interface IFile {
+  id: string;
+  name: string;
+  enabled: boolean;
+  last_update_utc?: string; // timestamp UTC
+  update_state?: string; // status of update - e.g. could not read | successful
+  last_version?: string; // latest file version
+  path: string;
+  mqtt_topic: string;
+}
+// subscribe to active files from sse backend
+const activeFiles = ref<IFile[]>([]);
+let eventSource = new EventSource("http://localhost:8000/api/files/sse");
+eventSource.onmessage = function (event) {
+  try {
+    let files: IFile[] = JSON.parse(event.data);
+    activeFiles.value = files.filter((file) => file.enabled);
+  } catch (error) {
+    console.error(`Could not update files: ${error}`);
+  }
+};
+// close eventsource on page leave
+onUnmounted(() => {
+  eventSource.close();
+});
 </script>
 <template>
   <div class="space-y-6 px-6 pt-8">
     <PanelMqttBroker />
-    <ListScraperInfo :scrapers="store.enabledFileScrapers" />
+    <ListScraperInfo :scrapers="activeFiles" />
   </div>
 </template>
