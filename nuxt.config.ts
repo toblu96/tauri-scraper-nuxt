b5e0fe3239e46697eca9acca3c971ae3aa3b624a
= <=>
2023-01-27 13:43:51 +0100

    Remove rest of pinia module references

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
-    modules: [
-        '@nuxtjs/tailwindcss',
-        [
-            '@pinia/nuxt',
-            {
-                autoImports: ['defineStore'],
-            }
-        ]
-    ],
-    alias: {
-        // due to https://github.com/nuxt/framework/issues/6623
-        // pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
+  ssr: false,
+  modules: ["@nuxtjs/tailwindcss"],
+  alias: {
+    // due to https://github.com/nuxt/framework/issues/6623
+    // pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
+  },
+  vite: {
+    build: {
+      target: "esnext",
     },
-    vite: {
-        build: {
-            target: "esnext"
-        }
-    }
-
-})
+  },
+});
